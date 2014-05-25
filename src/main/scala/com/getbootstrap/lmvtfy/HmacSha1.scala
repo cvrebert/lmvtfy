03d3652241a38ba736895e2f87a8a6068b3a6e65
Chris Rebert <code@rebertia.com>
2014-05-25 02:18:42 -0700

    add HmacSha1; oh god, crypto. I pray to DJB that this isn't buggy.

---
+++ src/main/scala/com/getbootstrap/lmvtfy/HmacSha1.scala
@@ -0,0 +1,35 @@
+package com.getbootstrap.lmvtfy
+
+import javax.crypto.Mac
+import javax.crypto.spec.SecretKeySpec
+import java.security.{NoSuchAlgorithmException, InvalidKeyException, SignatureException}
+import java.security.MessageDigest
+
+object HmacSha1 {
+  private val HmacSha1Algorithm = "HmacSHA1"
+
+  implicit class HexByteArray(array: Array[Byte]) {
+    import javax.xml.bind.DatatypeConverter
+    def asHexBytes: String = DatatypeConverter.printHexBinary(array).toLowerCase
+  }
+}
+
+case class HmacSha1(mac: Array[Byte], secretKey: Array[Byte], data: Array[Byte]) {
+  import HmacSha1.HmacSha1Algorithm
+  import HmacSha1.HexByteArray
+
+  @throws[NoSuchAlgorithmException]("if HMAC-SHA1 is not supported")
+  @throws[InvalidKeyException]("if the secret key is malformed")
+  @throws[SignatureException]("under unknown circumstances")
+  private lazy val correct: Array[Byte] = {
+    val key = new SecretKeySpec(secretKey, HmacSha1Algorithm)
+    val mac = Mac.getInstance(HmacSha1Algorithm)
+    mac.init(key)
+    mac.doFinal(data)
+  }
+
+  lazy val isValid: Boolean = MessageDigest.isEqual(mac, correct)
+
+  def givenHex = mac.asHexBytes
+  def correctHex = correct.asHexBytes
+}
