545d0cb737fd923f3b83680fac3813f02f0b0d12
Chris Rebert <code@rebertia.com>
2014-06-08 00:20:16 -0700

    add SourceSpanSpec

---
+++ src/test/scala/SourceSpanSpec.scala
@@ -0,0 +1,48 @@
+import org.specs2.mutable._
+import com.getbootstrap.lmvtfy.validation.{SourceLocation, SourceSpan}
+
+class SourceSpanSpec extends Specification {
+  "Start and end both invalid" should {
+    "be rejected" in {
+      SourceSpan(-1, 1, -2, 2) must beNone
+    }
+  }
+  "Only end valid" should {
+    "be reinterpreted as only start" in {
+      SourceSpan(-1, -1, 1, 2) must beSome.which{ span =>
+        (span.start mustEqual SourceLocation(1, 2).get) &&
+          (span.end must beNone)
+      }
+    }
+  }
+  "Only start valid" should {
+    "be accepted properly" in {
+      SourceSpan(2, 3, -1, -1) must beSome.which{ span =>
+        (span.start mustEqual SourceLocation(2, 3).get) &&
+        (span.end must beNone)
+      }
+    }
+    "stringify correctly" in {
+      SourceSpan(2, 3, -1, -1).get.toString mustEqual "line 2, column 3"
+    }
+  }
+  "Both start and end valid and in correct order" should {
+    "be accepted properly" in {
+      SourceSpan(2, 3, 4, 5) must beSome.which{ span =>
+        (span.start mustEqual SourceLocation(2, 3).get) &&
+          (span.end mustEqual Some(SourceLocation(4, 5).get))
+      }
+    }
+    "stringify correctly" in {
+      SourceSpan(2, 3, 4, 5).get.toString mustEqual "line 2, column 3 thru line 4, column 5"
+    }
+  }
+  "Both start and end valid but in wrong order" should {
+    "get flipped" in {
+      SourceSpan(4, 5, 2, 3) must beSome.which{ span =>
+        (span.start mustEqual SourceLocation(2, 3).get) &&
+          (span.end mustEqual Some(SourceLocation(4, 5).get))
+      }
+    }
+  }
+}
