1fae0da41c79155e60644996c6cc733c5ef1c21e
Chris Rebert <code@rebertia.com>
2014-06-08 22:01:07 -0700

    tweak Boot a bit

--- src/main/scala/com/getbootstrap/lmvtfy/server/Boot.scala
+++ src/main/scala/com/getbootstrap/lmvtfy/server/Boot.scala
@@ -9,10 +9,11 @@ import scala.concurrent.duration._
 
 object Boot extends App {
   implicit val system = ActorSystem("on-spray-can")
+  implicit val timeout = Timeout(5.seconds) // FIXME: ?
+  // import actorSystem.dispatcher
 
+  // FIXME: spawn actor for Html5Validator requests
   val service = system.actorOf(Props[LmvtfyActor], "lmvtfy-service")
 
-  implicit val timeout = Timeout(5.seconds)
-
-  IO(Http) ? Http.Bind(service, interface = "localhost", port = 80)
+  IO(Http) ? Http.Bind(service, interface = "0.0.0.0", port = 80)
 }
