31e73cb7cd2137b584434e9f817c302f05894fed
Chris Rebert <code@rebertia.com>
2014-06-09 00:54:06 -0700

    fix HMAC header parsing

--- src/main/scala/com/getbootstrap/lmvtfy/server/LmvtfyService.scala
+++ src/main/scala/com/getbootstrap/lmvtfy/server/LmvtfyService.scala
@@ -1,6 +1,5 @@
 package com.getbootstrap.lmvtfy.server
 
-import java.nio.charset.Charset
 import akka.actor.Actor
 import spray.routing._
 import spray.http._
@@ -8,6 +7,7 @@ import spray.json._
 import com.getbootstrap.lmvtfy.github._
 import akka.event.Logging
 import spray.routing.directives.DebuggingDirectives
+import scala.util.{Try, Success, Failure}
 
 class LmvtfyActor extends Actor with Lmvtfy {
   def actorRefFactory = context
@@ -25,42 +25,52 @@ trait Lmvtfy extends HttpService {
     path("lmvtfy") {
       post {
         headerValueByName("X-Github-Event") { githubEvent =>
-          headerValueByName("X-Hub-Signature") { hmacHex =>
-            val hmacBytes = javax.xml.bind.DatatypeConverter.parseHexBinary(hmacHex)
-            entity(as[Array[Byte]]) { rawBody =>
-              val FOOBARDDDDD = Array(1,2,3,4,5).map{_.toByte} // FIXME
-              val hmac = new HmacSha1(mac = hmacBytes, secretKey = FOOBARDDDDD, data = rawBody)
-              if (false && !hmac.isValid) {// FIXME
-                complete(StatusCodes.Forbidden, "HMAC verification failed!")
-              }
-              else {
-                formField("payload") { payload =>
-                  System.out.println("RAW JSON:", payload)
-                  githubEvent match {
-                    case "ping" => {
-                      System.out.println("Pong.")
-                      complete(StatusCodes.OK)
-                    }
-                    case "issues" | "issue_comment" => {
-                      val event = payload.parseJson.convertTo[IssueOrCommentEvent]
-                      event.action match {
-                        case "opened" | "created" => {
-                          val comment = event.comment.getOrElse(event.issue)
-                          val markdown = comment.body
-                          val username = comment.user.login
-                          System.out.println("EVENT: ", event.action)
-                          System.out.println("USERNAME: ", username)
-                          System.out.println("COMMENT BODY: ", markdown)
-                          // FIXME: DO ACTUAL WORK
-                          complete(StatusCodes.OK)
+          headerValueByName("X-Hub-Signature") { hmacStr =>
+            val parts = hmacStr.split('=')
+            parts match {
+              case Array("sha1", hmacHex) => {
+                Try{ javax.xml.bind.DatatypeConverter.parseHexBinary(hmacHex) } match {
+                  case Failure(_) => complete(StatusCodes.Forbidden, "Malformed HMAC hex!")
+                  case Success(hmacBytes) => {
+                    entity(as[Array[Byte]]) { rawBody =>
+                      val FOOBARDDDDD = Array(1,2,3,4,5).map{_.toByte} // FIXME
+                      val hmac = new HmacSha1(mac = hmacBytes, secretKey = FOOBARDDDDD, data = rawBody)
+                      if (false && !hmac.isValid) {// FIXME
+                        complete(StatusCodes.Forbidden, "HMAC verification failed!")
+                      }
+                      else {
+                        formField("payload") { payload =>
+                          System.out.println("RAW JSON:", payload)
+                          githubEvent match {
+                            case "ping" => {
+                              System.out.println("Pong.")
+                              complete(StatusCodes.OK)
+                            }
+                            case "issues" | "issue_comment" => {
+                              val event = payload.parseJson.convertTo[IssueOrCommentEvent]
+                              event.action match {
+                                case "opened" | "created" => {
+                                  val comment = event.comment.getOrElse(event.issue)
+                                  val markdown = comment.body
+                                  val username = comment.user.login
+                                  System.out.println("EVENT: ", event.action)
+                                  System.out.println("USERNAME: ", username)
+                                  System.out.println("COMMENT BODY: ", markdown)
+                                  // FIXME: DO ACTUAL WORK
+                                  complete(StatusCodes.OK)
+                                }
+                                case _ => complete(StatusCodes.OK, "Ignoring irrelevant action")
+                              }
+                            }
+                            case _ => complete(StatusCodes.BadRequest, "Unexpected event type")
+                          }
                         }
-                        case _ => complete(StatusCodes.OK, "Ignoring irrelevant action")
                       }
                     }
-                    case _ => complete(StatusCodes.BadRequest, "Unexpected event type")
                   }
                 }
               }
+              case _ => complete(StatusCodes.Forbidden, "Malformed HMAC!")
             }
           }
         }
