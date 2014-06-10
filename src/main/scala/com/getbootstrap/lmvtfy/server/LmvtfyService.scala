a742148d1dc01a3bae072e3bf17d32132a78a899
Chris Rebert <code@rebertia.com>
2014-06-09 20:14:53 -0700

    fix entity handling

--- src/main/scala/com/getbootstrap/lmvtfy/server/LmvtfyService.scala
+++ src/main/scala/com/getbootstrap/lmvtfy/server/LmvtfyService.scala
@@ -1,13 +1,14 @@
 package com.getbootstrap.lmvtfy.server
 
+import scala.util.{Try, Success, Failure}
 import akka.actor.Actor
 import spray.routing._
+import spray.routing.directives.DebuggingDirectives
 import spray.http._
 import spray.json._
 import com.getbootstrap.lmvtfy.github._
+import com.getbootstrap.lmvtfy.util.Utf8String
 import akka.event.Logging
-import spray.routing.directives.DebuggingDirectives
-import scala.util.{Try, Success, Failure}
 
 class LmvtfyActor extends Actor with Lmvtfy {
   def actorRefFactory = context
@@ -15,7 +16,6 @@ class LmvtfyActor extends Actor with Lmvtfy {
   // TODO: timeout handling
   def receive = runRoute(theOnlyRoute)
 }
-
 
 trait Lmvtfy extends HttpService {
   import GitHubJsonProtocol._
@@ -32,39 +32,37 @@ trait Lmvtfy extends HttpService {
                 Try{ javax.xml.bind.DatatypeConverter.parseHexBinary(hmacHex) } match {
                   case Failure(_) => complete(StatusCodes.Forbidden, "Malformed HMAC hex!")
                   case Success(hmacBytes) => {
-                    //entity(as[Array[Byte]]) { rawBody =>
-                      val FOOBARDDDDD = Array(1,2,3,4,5).map{_.toByte} // FIXME
-                      val hmac = new HmacSha1(mac = hmacBytes, secretKey = FOOBARDDDDD, data = FOOBARDDDDD/*rawBody*/)
-                      if (false && !hmac.isValid) {// FIXME
-                        complete(StatusCodes.Forbidden, "HMAC verification failed!")
+                    githubEvent match {
+                      case "ping" => {
+                        System.out.println("Pong.")
+                        complete(StatusCodes.OK)
                       }
-                      else {
-                        githubEvent match {
-                          case "ping" => {
-                            System.out.println("Pong.")
-                            complete(StatusCodes.OK)
+                      case "issues" | "issue_comment" => {  context =>
+                        entity(as[String]) { stringEntity =>
+                          val FOOBARDDDDD = Array(1,2,3,4,5).map{_.toByte} // FIXME
+                          val hmac = new HmacSha1(mac = hmacBytes, secretKey = FOOBARDDDDD, data = stringEntity.utf8Bytes)
+                          if (false && !hmac.isValid) {// FIXME
+                            complete(StatusCodes.Forbidden, "HMAC verification failed!")
                           }
-                          case "issues" | "issue_comment" => {  context =>
-                            entity(as[String]) { stringEntity =>
-                              Try{ stringEntity.parseJson.convertTo[IssueOrCommentEvent] } match {
-                                case Failure(_) => complete(StatusCodes.BadRequest, "JSON either malformed or does not match expected schema!")
-                                case Success(event) => {
-                                  event.action match {
-                                    case "opened" | "created" => {
-                                      System.out.println("EVENT: ", event)
-                                      // FIXME: DO ACTUAL WORK
-                                      complete(StatusCodes.OK)
-                                    }
-                                    case _ => complete(StatusCodes.OK, "Ignoring irrelevant action")
+                          else {
+                            Try{ stringEntity.parseJson.convertTo[IssueOrCommentEvent] } match {
+                              case Failure(_) => complete(StatusCodes.BadRequest, "JSON either malformed or does not match expected schema!")
+                              case Success(event) => {
+                                event.action match {
+                                  case "opened" | "created" => {
+                                    System.out.println("EVENT: ", event)
+                                    // FIXME: DO ACTUAL WORK
+                                    complete(StatusCodes.OK)
                                   }
+                                  case _ => complete(StatusCodes.OK, "Ignoring irrelevant action")
                                 }
                               }
                             }
                           }
-                          case _ => complete(StatusCodes.BadRequest, "Unexpected event type")
                         }
                       }
-                    //}
+                      case _ => complete(StatusCodes.BadRequest, "Unexpected event type")
+                    }
                   }
                 }
               }
