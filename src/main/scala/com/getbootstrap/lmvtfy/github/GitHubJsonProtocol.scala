53f705544e4c1f268dfb052a869c6db76f1c15a4
Chris Rebert <code@rebertia.com>
2014-06-09 20:02:49 -0700

    extract GitHub repo name from WebHook payload too

--- src/main/scala/com/getbootstrap/lmvtfy/github/GitHubJsonProtocol.scala
+++ src/main/scala/com/getbootstrap/lmvtfy/github/GitHubJsonProtocol.scala
@@ -2,6 +2,7 @@ package com.getbootstrap.lmvtfy.github
 
 import spray.json.DefaultJsonProtocol
 
+case class GitHubRepository(full_name: String)
 case class GitHubUser(login: String)
 case class IssueOrComment(
   body: String,
@@ -9,12 +10,14 @@ case class IssueOrComment(
 )
 case class IssueOrCommentEvent(
   action: String, // issue_comment: "opened", "closed", "reopened"; issue: "created"
+  repo: GitHubRepository,
   comment: Option[IssueOrComment],
   issue: IssueOrComment
 )
 
 object GitHubJsonProtocol extends DefaultJsonProtocol {
+  implicit val gitHubRepoFormat = jsonFormat1(GitHubRepository.apply)
   implicit val gitHubUserFormat = jsonFormat1(GitHubUser.apply)
   implicit val issueOrCommentFormat = jsonFormat2(IssueOrComment.apply)
-  implicit val issueOrCommentEventFormat = jsonFormat3(IssueOrCommentEvent.apply)
+  implicit val issueOrCommentEventFormat = jsonFormat4(IssueOrCommentEvent.apply)
 }
