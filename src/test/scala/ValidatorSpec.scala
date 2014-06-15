a11ea3e80c08133b30bcff918fbeb5c4f3ec64cb
Chris Rebert <code@rebertia.com>
2014-06-15 00:36:33 -0700

    rm excess whitespace from HTML strings

--- src/test/scala/ValidatorSpec.scala
+++ src/test/scala/ValidatorSpec.scala
@@ -9,21 +9,20 @@ class ValidatorSpec extends Specification {
   }
 
   val httpEquivErrText = Vector(PlainText("Bad value "), CodeText("Gibberish"), PlainText(" for attribute "), CodeText("http-equiv"), PlainText(" on XHTML element "), CodeText("meta"), PlainText("."))
-  val httpEquivErrSpan = SourceSpan(6, 57, 6, 57).get
+  val httpEquivErrSpan = SourceSpan(5, 56, 5, 56).get
   val httpEquivValidationMsg = ValidationMessage(Some(httpEquivErrSpan), httpEquivErrText)
 
   "Bad meta http-equiv" should {
     val badHtml =
-      """
-        | <!DOCTYPE html>
-        | <html lang="en">
-        |   <head>
-        |     <meta charset="utf-8">
-        |     <meta http-equiv="Gibberish" content="gobbledegook">
-        |     <title>Title</title>
-        |   </head>
-        |   <body></body>
-        | </html>
+      """<!DOCTYPE html>
+        |<html lang="en">
+        |  <head>
+        |    <meta charset="utf-8">
+        |    <meta http-equiv="Gibberish" content="gobbledegook">
+        |    <title>Title</title>
+        |  </head>
+        |  <body></body>
+        |</html>
       """.stripMargin
 
     "cause a validation error" in {
@@ -35,24 +34,23 @@ class ValidatorSpec extends Specification {
 
   "Bad meta http-equiv and non-table-related child of a table" should {
     val badHtml =
-      """
-        | <!DOCTYPE html>
-        | <html lang="en">
-        |   <head>
-        |     <meta charset="utf-8">
-        |     <meta http-equiv="Gibberish" content="gobbledegook">
-        |     <title>Title</title>
-        |   </head>
-        |   <body>
-        |     <ul>
-        |       <p>Can't just randomly put a paragraph tag here!</p>
-        |     </ul>
-        |   </body>
-        | </html>
+      """<!DOCTYPE html>
+        |<html lang="en">
+        |  <head>
+        |    <meta charset="utf-8">
+        |    <meta http-equiv="Gibberish" content="gobbledegook">
+        |    <title>Title</title>
+        |  </head>
+        |  <body>
+        |    <ul>
+        |      <p>Can't just randomly put a paragraph tag here!</p>
+        |    </ul>
+        |  </body>
+        |</html>
       """.stripMargin
 
     "cause 2 validation errors" in {
-      val pInUlMsg = ValidationMessage(SourceSpan(11, 10, -1, -1), Vector(PlainText("HTML element "), CodeText("p"), PlainText(" not allowed as child of HTML element "), CodeText("ul"), PlainText(" in this context. (Suppressing further errors from this subtree.)")))
+      val pInUlMsg = ValidationMessage(SourceSpan(10, 9, -1, -1), Vector(PlainText("HTML element "), CodeText("p"), PlainText(" not allowed as child of HTML element "), CodeText("ul"), PlainText(" in this context. (Suppressing further errors from this subtree.)")))
 
       val messages = Html5Validator.validationErrorsFor(badHtml.inputSource)
       messages must have size(2)
