bb04a6a684e348a9973c0e44c9ada28c2b9a8371
Chris Rebert <code@rebertia.com>
2014-06-15 19:29:49 -0700

    use port 8080 instead, because permissions

--- src/main/scala/com/chrisrebert/lmvtfy/server/Boot.scala
+++ src/main/scala/com/chrisrebert/lmvtfy/server/Boot.scala
@@ -1,6 +1,7 @@
 package com.chrisrebert.lmvtfy.server
 
 import scala.concurrent.duration._
+import scala.util.Try
 import akka.actor.{ActorSystem, Props}
 import akka.io.IO
 import spray.can.Http
@@ -11,15 +12,32 @@ import com.chrisrebert.lmvtfy.github.GitHubIssueCommenter
 
 
 object Boot extends App {
-  implicit val system = ActorSystem("on-spray-can")
-  // import actorSystem.dispatcher
+  val arguments = args.toSeq
+  val maybePort = arguments match {
+    case Seq(portStr: String) => {
+      Try{ portStr.toInt }.toOption
+    }
+    case _ => None
+  }
+  maybePort match {
+    case Some(port) => run(port)
+    case _ => {
+      System.err.println("USAGE: lmvtfy <port-number>")
+      System.exit(1)
+    }
+  }
 
-  val commenter = system.actorOf(Props(classOf[GitHubIssueCommenter]))
-  val localValidator = system.actorOf(Props(classOf[ValidatorSingletonActor], commenter), "validator-service")
-  val exampleFetcherPool = system.actorOf(SmallestMailboxPool(5).props(Props(classOf[LiveExampleFetcher], localValidator)), "example-fetcher-pool")
-  val issueCommentEventHandler = system.actorOf(Props(classOf[IssueCommentEventHandler], exampleFetcherPool), "issue-comment-event-handler")
-  val webService = system.actorOf(Props(classOf[LmvtfyActor], issueCommentEventHandler), "lmvtfy-service")
+  def run(port: Int) {
+    implicit val system = ActorSystem("on-spray-can")
+    // import actorSystem.dispatcher
 
-  implicit val timeout = Timeout(15.seconds)
-  IO(Http) ? Http.Bind(webService, interface = "0.0.0.0", port = 80)
+    val commenter = system.actorOf(Props(classOf[GitHubIssueCommenter]))
+    val localValidator = system.actorOf(Props(classOf[ValidatorSingletonActor], commenter), "validator-service")
+    val exampleFetcherPool = system.actorOf(SmallestMailboxPool(5).props(Props(classOf[LiveExampleFetcher], localValidator)), "example-fetcher-pool")
+    val issueCommentEventHandler = system.actorOf(Props(classOf[IssueCommentEventHandler], exampleFetcherPool), "issue-comment-event-handler")
+    val webService = system.actorOf(Props(classOf[LmvtfyActor], issueCommentEventHandler), "lmvtfy-service")
+
+    implicit val timeout = Timeout(15.seconds)
+    IO(Http) ? Http.Bind(webService, interface = "0.0.0.0", port = port)
+  }
 }
