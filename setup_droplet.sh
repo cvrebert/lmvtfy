b4bc6340d0f496d38b7625962675357e79153996
Chris Rebert <code@rebertia.com>
2014-06-15 20:43:33 -0700

    more setup automation

--- setup_droplet.sh
+++ setup_droplet.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Step 0: You need to have copied the assembly JAR to lmvtfy/target/scala-2.10/lmvtfy-assembly-1.0.jar
+
 # set to Pacific Time
 ln -sf /usr/share/zoneinfo/America/Los_Angeles /etc/localtime
 
@@ -12,3 +14,10 @@ ufw default allow outgoing
 ufw default deny incoming
 ufw allow ssh
 ufw allow www
+ufw enable
+ufw status verbose
+
+# setup Docker
+docker build . 2>&1 | tee > docker.build.log
+IMAGE_ID="$(tail -n 1 docker.build.log)"
+docker run -d -p 80:8080 $IMAGE_ID
