1b91c652c151253de7407dcec8de2e6efc7551e5
Chris Rebert <code@rebertia.com>
2014-06-23 22:41:47 -0700

    fix ensuing spelling errors from validator

--- src/main/scala/com/chrisrebert/lmvtfy/validation/MessagePart.scala
+++ src/main/scala/com/chrisrebert/lmvtfy/validation/MessagePart.scala
@@ -21,11 +21,14 @@ case class CodeText(text: String) extends MessagePart
 object SpellingErrorCorrector {
   import java.util.regex.Pattern
 
-  private val pattern = Pattern.compile("^Aattribute\\b", Pattern.UNICODE_CHARACTER_CLASS)
+  private val attrPattern = Pattern.compile("^Aattribute\\b", Pattern.UNICODE_CHARACTER_CLASS)
+  private val elemPattern = Pattern.compile("^Eelement\\b", Pattern.UNICODE_CHARACTER_CLASS)
 
   /**
    * Corrects known spelling errors in the validator's messages
    * @param str
    */
-  def correct(str: String): String = pattern.matcher(str).replaceFirst("Attribute")
+  def correct(str: String): String = correctElement(correctAttribute(str))
+  private def correctAttribute(str: String) = attrPattern.matcher(str).replaceFirst("Attribute")
+  private def correctElement(str: String) = elemPattern.matcher(str).replaceFirst("Element")
 }
