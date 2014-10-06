bf4ceeb151fdb04e6c7e3bc913c02597d265b671
Chris Rebert <code@rebertia.com>
2014-10-06 15:05:46 -0700

    Fixes #52

--- src/main/scala/com/chrisrebert/lmvtfy/validation/MessagePart.scala
+++ src/main/scala/com/chrisrebert/lmvtfy/validation/MessagePart.scala
@@ -1,16 +1,6 @@
 package com.chrisrebert.lmvtfy.validation
 
 sealed trait MessagePart
-object PlainText {
-  def apply(text: String): PlainText = {
-    new PlainText(text)
-  }
-  def unapply(plainText: PlainText) = Some(plainText.text)
-}
-class PlainText private(val text: String) extends MessagePart {
-  override def equals(other: Any) = other.isInstanceOf[PlainText] && other.asInstanceOf[PlainText].text == text
-  override def hashCode = text.hashCode
-  override def toString = s"PlainText(${text})"
-}
+case class PlainText(text: String) extends MessagePart
 case class Link(url: String, title: String) extends MessagePart
 case class CodeText(text: String) extends MessagePart
