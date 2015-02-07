27612a16637ecd087e8e2bd0bcac48044755349b
Chris Rebert <github@rebertia.com>
2015-02-06 17:48:52 -0800

    Dockerfile: update Docker version

--- setup_droplet.sh
+++ setup_droplet.sh
@@ -17,7 +17,7 @@ ufw allow www
 ufw enable
 ufw status verbose
 
-# setup Docker; written against Docker v1.0.0
+# setup Docker; written against Docker v1.5.0
 docker build . 2>&1 | tee docker.build.log
 IMAGE_ID="$(tail -n 1 docker.build.log | cut -d ' ' -f 3)"
 docker run --read-only -d -p 80:8080 --link bootlint:bootlint --name lmvtfy $IMAGE_ID
